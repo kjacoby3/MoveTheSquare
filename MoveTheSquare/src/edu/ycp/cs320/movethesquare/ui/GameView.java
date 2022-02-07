93134bf09e65c2f15fad9a90c9807e8de38755a0
Koen Jacoby <kjacoby3@ycp.edu>
2022-02-07 12:16:13 -0500

    Commit Test

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -82,7 +82,7 @@ public class GameView extends JPanel {
 				model.setHeight(480.0);
 				
 				Square square = new Square();
-				square.setX(350.0);
+				square.setX(300.0);
 				square.setY(220.0);
 				square.setWidth(40.0);
 				square.setHeight(40.0);
